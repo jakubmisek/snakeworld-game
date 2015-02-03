7097779bea27f1f904990ecbb61d4163d1a05307
Jakub <jmisek@gmail.com>
2015-02-03 14:32:47 +0000

    - (R) added to registered users name - fixed login - plays count
    Originally checked in by DEVSENSE\Administrator on 2011-02-20 12:13:36.04 as Changeset 946

--- SnakeWorld Server Engine/snakeworld.designer.cs
+++ SnakeWorld Server Engine/snakeworld.designer.cs
@@ -82,9 +82,15 @@ namespace SnakeWorld_Server
 		
 		private int _userId;
 		
-		private int _timeMinutesPlayed;
+		private int _timeSecondsPlayed;
 		
 		private double _maxLength;
+		
+		private int _kills;
+		
+		private int _suicides;
+		
+		private int _plays;
 		
     #region Extensibility Method Definitions
     partial void OnLoaded();
@@ -92,10 +98,16 @@ namespace SnakeWorld_Server
     partial void OnCreated();
     partial void OnuserIdChanging(int value);
     partial void OnuserIdChanged();
-    partial void OntimeMinutesPlayedChanging(int value);
-    partial void OntimeMinutesPlayedChanged();
+    partial void OntimeSecondsPlayedChanging(int value);
+    partial void OntimeSecondsPlayedChanged();
     partial void OnmaxLengthChanging(double value);
     partial void OnmaxLengthChanged();
+    partial void OnkillsChanging(int value);
+    partial void OnkillsChanged();
+    partial void OnsuicidesChanging(int value);
+    partial void OnsuicidesChanged();
+    partial void OnplaysChanging(int value);
+    partial void OnplaysChanged();
     #endregion
 		
 		public SnakeInfo()
@@ -123,22 +135,22 @@ namespace SnakeWorld_Server
 			}
 		}
 		
-		[Column(Storage="_timeMinutesPlayed", DbType="Int NOT NULL")]
-		public int timeMinutesPlayed
+		[Column(Storage="_timeSecondsPlayed", DbType="Int NOT NULL")]
+		public int timeSecondsPlayed
 		{
 			get
 			{
-				return this._timeMinutesPlayed;
+				return this._timeSecondsPlayed;
 			}
 			set
 			{
-				if ((this._timeMinutesPlayed != value))
+				if ((this._timeSecondsPlayed != value))
 				{
-					this.OntimeMinutesPlayedChanging(value);
+					this.OntimeSecondsPlayedChanging(value);
 					this.SendPropertyChanging();
-					this._timeMinutesPlayed = value;
-					this.SendPropertyChanged("timeMinutesPlayed");
-					this.OntimeMinutesPlayedChanged();
+					this._timeSecondsPlayed = value;
+					this.SendPropertyChanged("timeSecondsPlayed");
+					this.OntimeSecondsPlayedChanged();
 				}
 			}
 		}
@@ -159,6 +171,66 @@ namespace SnakeWorld_Server
 					this._maxLength = value;
 					this.SendPropertyChanged("maxLength");
 					this.OnmaxLengthChanged();
+				}
+			}
+		}
+		
+		[Column(Storage="_kills", DbType="Int NOT NULL")]
+		public int kills
+		{
+			get
+			{
+				return this._kills;
+			}
+			set
+			{
+				if ((this._kills != value))
+				{
+					this.OnkillsChanging(value);
+					this.SendPropertyChanging();
+					this._kills = value;
+					this.SendPropertyChanged("kills");
+					this.OnkillsChanged();
+				}
+			}
+		}
+		
+		[Column(Storage="_suicides", DbType="Int NOT NULL")]
+		public int suicides
+		{
+			get
+			{
+				return this._suicides;
+			}
+			set
+			{
+				if ((this._suicides != value))
+				{
+					this.OnsuicidesChanging(value);
+					this.SendPropertyChanging();
+					this._suicides = value;
+					this.SendPropertyChanged("suicides");
+					this.OnsuicidesChanged();
+				}
+			}
+		}
+		
+		[Column(Storage="_plays", DbType="Int NOT NULL")]
+		public int plays
+		{
+			get
+			{
+				return this._plays;
+			}
+			set
+			{
+				if ((this._plays != value))
+				{
+					this.OnplaysChanging(value);
+					this.SendPropertyChanging();
+					this._plays = value;
+					this.SendPropertyChanged("plays");
+					this.OnplaysChanged();
 				}
 			}
 		}
