d987101388421a6e061ccddfd05bbcf03070d282
Jakub <jmisek@gmail.com>
2015-02-03 14:32:50 +0000

    - SnakeWorld Highscore Web
    Originally checked in by DEVSENSE\Administrator on 2011-02-20 12:13:36.697 as Changeset 947

---
+++ SnakeWorld Highscore Web/Default.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    public class ResultInfo
+    {
+        private string _name;
+        private int _length, _kills, _suicides, _plays;
+        private TimeSpan _playtime;
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public int length
+        {
+            get
+            {
+                return _length;
+            }
+        }
+        public int kills
+        {
+            get
+            {
+                return _kills;
+            }
+        }
+        public int suicides
+        {
+            get
+            {
+                return _suicides;
+            }
+        }
+        public int plays
+        {
+            get
+            {
+                return _plays;
+            }
+        }
+        public TimeSpan playtime
+        {
+            get
+            {
+                return _playtime;
+            }
+        }
+
+
+        public ResultInfo(UserInfo user, SnakeInfo snake)
+        {
+            _name = user.name;
+            _length = (int)snake.maxLength;
+            _kills = snake.kills;
+            _suicides = snake.suicides;
+            _plays = snake.plays;
+            _playtime = TimeSpan.FromSeconds((double)snake.timeSecondsPlayed);
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+            return;
+
+        var snakeworldDb = new snakeworldDataContext();
+        var webDb = new webDataContext();
+
+        var users = (from a in snakeworldDb.SnakeInfos
+                     orderby a.maxLength descending
+                     select a);
+
+        List<ResultInfo> resultInfo = new List<ResultInfo>();
+
+        foreach (var v in users)
+        {
+            var user = webDb.UserInfos.SingleOrDefault(u => u.userId == v.userId);
+            if (user != null)
+                resultInfo.Add(new ResultInfo(user, v));
+        }
+
+        results.DataSource = resultInfo;
+        results.DataBind();
+    }
+
+}
